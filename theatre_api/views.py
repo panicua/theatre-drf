0d8e998067604e6b943647b4d0e216ba65c929e0
kekW <panicua12@gmail.com>
2024-06-06 20:05:38 +0300

    add: views for genres and actors

--- theatre_api/views.py
+++ theatre_api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from theatre_api.models import Genre, Actor
+from theatre_api.permissions import IsAdminOrIfAuthenticatedReadOnly
+from theatre_api.serializers import GenreSerializer, ActorSerializer
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
