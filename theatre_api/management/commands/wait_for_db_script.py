2f5ac0ec8affa4b6bbb35f7ba7ac269594488795
kekW <panicua12@gmail.com>
2024-06-08 22:08:27 +0300

    fix: changed script to wait for db start when using docker correctly

--- theatre_api/management/commands/wait_for_db_script.py
+++ theatre_api/management/commands/wait_for_db_script.py
@@ -1,25 +1,27 @@
 import time
-
 from django.core.management.base import BaseCommand
-from django.db import connections
+from django.db import connections, OperationalError
 
 
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
-        self.stdout.write("Waiting for database... 10 sec till timeout")
-        timeout_wait = 0
-        db_connection = None
-        while not db_connection and timeout_wait < 10:
-            timeout_wait += 1
-            try:
-                db_connection = connections["default"]
-            except Exception:
-                self.stdout.write(
-                    f"Database unavailable, waiting {timeout_wait} second...")
-                time.sleep(1)
-            if timeout_wait == 10:
-                raise Exception("Database unavailable")
+        self.stdout.write("Waiting for database...")
+        db_conn = None
+        attempts = 0
+        max_attempts = 20
 
-        self.stdout.write(self.style.SUCCESS("Database available!"))
+        while attempts < max_attempts:
+            try:
+                db_conn = connections["default"]
+                c = db_conn.cursor()
+                c.execute("SELECT 1;")
+                self.stdout.write(self.style.SUCCESS("Database available!"))
+                return
+            except OperationalError:
+                attempts += 1
+                self.stdout.write(f"Database unavailable, waiting {attempts} second(s)...")
+                time.sleep(1)
+
+        raise Exception("Database unavailable after waiting for 20 seconds")
