92eb3224f50568e51b56f8ac9b414f5558469145
kekW <panicua12@gmail.com>
2024-06-12 18:29:11 +0300

    chore/ref: moved duplicated code to separate funcs

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -13,14 +13,36 @@ WEBSITE_BASE_URL = config("WEBSITE_BASE_URL")
 
 
 async def start(update: Update, context: CallbackContext) -> None:
+    reply_markup = create_reply_markup()
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+
+
+def create_reply_markup() -> InlineKeyboardMarkup:
     keyboard = [
         [InlineKeyboardButton("Genres", callback_data="genres")],
         [InlineKeyboardButton("Actors", callback_data="actors")],
     ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text(
-        "Please choose:", reply_markup=reply_markup
-    )
+    return InlineKeyboardMarkup(keyboard)
+
+
+def fetch_data(endpoint: str) -> str:
+    response = requests.get(f"{WEBSITE_BASE_URL}/api/theatre/{endpoint}/")
+    answer_string = text = ""
+    for result in response.json()["results"]:
+        if endpoint == "genres":
+            text = f"*name:* {result['name']}"
+        elif endpoint == "actors":
+            text = f"*full_name:* {result['full_name']}"
+        answer_string += f"{text}\n"
+    return answer_string
+
+
+async def handle_response(query, endpoint: str) -> None:
+    try:
+        answer_string = fetch_data(endpoint)
+        await query.edit_message_text(text=answer_string, parse_mode="markdown")
+    except Exception as e:
+        await query.edit_message_text(text=str(e))
 
 
 async def button(update: Update, context: CallbackContext) -> None:
@@ -28,37 +50,10 @@ async def button(update: Update, context: CallbackContext) -> None:
     await query.answer()
 
     choice = query.data
-    if choice == "genres":
-        response = requests.get(f"{WEBSITE_BASE_URL}/api/theatre/genres/")
-        answer_string = ""
-        for result in response.json()["results"]:
-            text = f"*name:* {result['name']}"
-            answer_string += f"{text}\n"
-        try:
-            await query.edit_message_text(
-                text=f"{answer_string}", parse_mode="markdown"
-            )
-        except Exception as e:
-            await query.edit_message_text(text=f"{e}")
+    if choice in ["genres", "actors"]:
+        await handle_response(query, choice)
 
-    elif choice == "actors":
-        response = requests.get(f"{WEBSITE_BASE_URL}/api/theatre/actors/")
-        answer_string = ""
-        for result in response.json()["results"]:
-            text = f"*full_name:* {result['full_name']}"
-            answer_string += f"{text}\n"
-        try:
-            await query.edit_message_text(
-                text=f"{answer_string}", parse_mode="markdown"
-            )
-        except Exception as e:
-            await query.edit_message_text(text=f"{e}")
-
-    keyboard = [
-        [InlineKeyboardButton("Genres", callback_data="genres")],
-        [InlineKeyboardButton("Actors", callback_data="actors")],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    reply_markup = create_reply_markup()
     await query.message.edit_reply_markup(reply_markup)
 
 
