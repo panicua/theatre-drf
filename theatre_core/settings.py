2e191bc131852af5be0cc5ad04b1820d252547cb
kekW <panicua12@gmail.com>
2024-06-12 18:30:17 +0300

    ref: DEBUG,ALLOWED_HOSTS moved to .env

--- theatre_core/settings.py
+++ theatre_core/settings.py
@@ -6,13 +6,13 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["localhost", "theatre-app", "127.0.0.1"]
+ALLOWED_HOSTS = config(
+    "DJANGO_ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")]
+)
 
 INTERNAL_IPS = [
     "127.0.0.1",
